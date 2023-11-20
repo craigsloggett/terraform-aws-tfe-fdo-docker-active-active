8676792269129d80d7a7f5bfa52b778794bee418
Craig Sloggett <craigsloggett@gmail.com>
2023-11-20 11:01:26 -0500

    Add VPC networking

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "703951826048-ca-central-1-terraform-state"
+    dynamodb_table = "703951826048-ca-central-1-terraform-state-locks"
+    key            = "terraform.tfstate"
+    encrypt        = "true"
+    kms_key_id     = "alias/aws/s3"
+    region         = "ca-central-1"
+  }
+}
