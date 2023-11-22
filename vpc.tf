6444a75b2bb04022b60978aae30de88386ede8b9
Craig Sloggett <craigsloggett@gmail.com>
2023-11-22 18:23:55 -0500

    Rename network.tf to vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,49 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.2.0"
+
+  name = "tfe-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  default_network_acl_egress = [{
+    "action" : "allow",
+    "cidr_block" : "0.0.0.0/0",
+    "from_port" : 0,
+    "protocol" : "-1",
+    "rule_no" : 100, "to_port" : 0
+  }]
+
+  default_network_acl_ingress = [{
+    "action" : "allow",
+    "cidr_block" : "0.0.0.0/0",
+    "from_port" : 0,
+    "protocol" : "-1",
+    "rule_no" : 100, "to_port" : 0
+  }]
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = "com.amazonaws.ca-central-1.s3"
+
+  tags = {
+    Name = "tfe-vpce-s3"
+  }
+}
+
+resource "aws_vpc_endpoint_route_table_association" "public" {
+  route_table_id  = module.vpc.public_route_table_ids[0]
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private" {
+  route_table_id  = module.vpc.private_route_table_ids[0]
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
