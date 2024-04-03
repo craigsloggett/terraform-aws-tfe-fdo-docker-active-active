2c0ae98a6b1d823b7ba08ec8c13fb9909171ab7f
Craig Sloggett <craigsloggett@gmail.com>
2024-04-03 15:44:11 -0400

    Use main.tf to remove linting error

--- locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  account_id  = data.aws_caller_identity.current.account_id
-  region      = data.aws_region.current.name
-  bucket_name = "${local.account_id}-${local.region}-terraform-enterprise"
-  my_ip       = chomp(data.http.myip.response_body)
-}
