5bf9ab18db87a793f50e0b2efed420cfb94ecb45
Craig Sloggett <craigsloggett@gmail.com>
2024-04-03 16:00:59 -0400

    Add dependabot and linting (#1)
    * Add dependabot and linting
    
    * Add terraform init command
    
    * Disable a backend for linting
    
    * Use main.tf to remove linting error
    
    * Remove non-working cache

--- main.tf
+++ main.tf
@@ -42,3 +42,11 @@ data "aws_secretsmanager_secret" "tfe_license" {
 data "aws_secretsmanager_secret" "tfe_encryption_password" {
   name = "tfe/encryption_password"
 }
+
+locals {
+  account_id  = data.aws_caller_identity.current.account_id
+  region      = data.aws_region.current.name
+  bucket_name = "${local.account_id}-${local.region}-terraform-enterprise"
+  my_ip       = chomp(data.http.myip.response_body)
+}
+
