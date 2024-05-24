442daa66bddd33796f2cfcce858ec03b2c5eb70a
Craig Sloggett <craigsloggett@gmail.com>
2024-05-24 16:36:42 -0400

    Use the aws_acm_certificate_validation resource to grab the certificate

--- alb.tf
+++ alb.tf
@@ -12,7 +12,7 @@ resource "aws_lb_listener" "tfe" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.tfe.arn
+  certificate_arn   = aws_acm_certificate_validation.tfe.certificate_arn
 
   default_action {
     type             = "forward"
