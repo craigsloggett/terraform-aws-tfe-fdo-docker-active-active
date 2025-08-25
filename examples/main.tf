6d0a056621bd8def1714c82a19556d56ba015654
Craig Sloggett <craigsloggett@gmail.com>
2025-08-25 16:06:16 -0400

    chore: Update example to use tfe_version as a required variable

--- examples/main.tf
+++ examples/main.tf
@@ -3,10 +3,7 @@ module "tfe" {
 
   # Required
   tfe_license                = var.tfe_license
+  tfe_version                = var.tfe_version
   route53_zone_name          = var.route53_zone_name
   ec2_bastion_ssh_public_key = var.ec2_bastion_ssh_public_key
-
-  # Optional
-  tfe_version = var.tfe_version
 }
-
